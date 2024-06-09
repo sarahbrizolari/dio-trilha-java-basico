a848bda65fbc888a1496a414963426b1bee5e82b
sarah <sarahbrizolari@gmail.com>
2024-06-09 12:26:25 -0300

    criando excecoes try, catch

---
+++ excecoes/src/FormatdorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatdorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep não corresponde com aa regras de negocio");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+    
+}
